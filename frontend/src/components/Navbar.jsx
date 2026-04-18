dbcbfadcce8d78cdd7685142f1e5f0afd49641bc
shivani malgaya <shivanimalgaya10@gmail.com>
2026-04-18 12:45:44 +0530

    first commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
               `}
             >
               <Settings className="w-4 h-4" />
-              <span className="hidden sm:inline">Settings</span>
+              <span className="hidden sm:inline">Settings </span>
             </Link>
 
             {authUser && (
